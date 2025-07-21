8ac95279767e09f0f6d835eef76edf38de99ed03
amalige <amalige@bnl.gov>
2025-07-21 12:59:33 -0400

    adding system int files

--- aieml/graph.h
+++ aieml/graph.h
@@ -51,9 +51,6 @@ public:
     input_plio  pl_w_dense1;
     output_plio pl_out_dense1;
 
-    input_plio relu_in_plio;
-    output_plio relu_out_plio[TP_CASC_LEN_LAYER2];
-
     input_plio  pl_in_dense2[TP_CASC_LEN_LAYER2];
     input_plio  pl_w_dense2[TP_CASC_LEN_LAYER2];
     output_plio pl_out_dense2;
@@ -64,7 +61,7 @@ public:
     NeuralNetworkGraph() {
         pl_in_dense1  = input_plio::create("plio_input_dense1", plio_32_bits, "data/input_data.txt");
         pl_w_dense1   = input_plio::create("plio_weights_dense1", plio_32_bits, "data/weights_dense1.txt");
-        pl_out_dense1 = output_plio::create("plio_output_dense1", plio_32_bits, "data/dense1_output_aie.txt");
+        // pl_out_dense1 = output_plio::create("plio_output_dense1", plio_32_bits, "data/dense1_output_aie.txt");
 
         connect<>(pl_w_dense1.out[0], dense1.inA[0]);
         connect<>(pl_in_dense1.out[0], dense1.inB[0]);
@@ -72,14 +69,15 @@ public:
 
         for (int i = 0; i < TP_CASC_LEN_LAYER2; ++i) {
             std::string in_file = "data/leakyrelu_output_part" + std::to_string(i) + ".txt";
-            std::string w_file  = "data/weights_dense2_part" + std::to_string(i) + ".txt";
+            // std::string w_file  = "data/weights_dense2_part" + std::to_string(i) + ".txt";
 
-            std::string in_name = "plio_input_dense2_" + std::to_string(i);
+            // std::string in_name = "plio_input_dense2_" + std::to_string(i);
             std::string w_name  = "plio_weights_dense2_" + std::to_string(i);
 
-            pl_in_dense2[i] = input_plio::create(in_name.c_str(), plio_32_bits, in_file.c_str());
+            // pl_in_dense2[i] = input_plio::create(in_name.c_str(), plio_32_bits, in_file.c_str());
             pl_w_dense2[i]  = input_plio::create(w_name.c_str(), plio_32_bits, w_file.c_str());
 
+            // connect<>(pl_in_dense2[i].out[0], dense2.inB[i]);
             connect<>(pl_in_dense2[i].out[0], dense2.inB[i]);
             connect<>(pl_w_dense2[i].out[0], dense2.inA[i]);
         }
