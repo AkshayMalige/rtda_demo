d161de55697a36e6539a0524b3d1dca8440fa075
amalige <amalige@bnl.gov>
2025-07-28 11:21:36 -0400

    working sim for all independent compoenents on vitis gui

--- aieml/graph.h
+++ aieml/graph.h
@@ -61,7 +61,7 @@ public:
     NeuralNetworkGraph() {
         pl_in_dense1  = input_plio::create("plio_input_dense1", plio_32_bits, "data/input_data.txt");
         pl_w_dense1   = input_plio::create("plio_weights_dense1", plio_32_bits, "data/weights_dense1.txt");
-        // pl_out_dense1 = output_plio::create("plio_output_dense1", plio_32_bits, "data/dense1_output_aie.txt");
+        pl_out_dense1 = output_plio::create("plio_output_dense1", plio_32_bits, "data/dense1_output_aie.txt");
 
         connect<>(pl_w_dense1.out[0], dense1.inA[0]);
         connect<>(pl_in_dense1.out[0], dense1.inB[0]);
@@ -69,15 +69,14 @@ public:
 
         for (int i = 0; i < TP_CASC_LEN_LAYER2; ++i) {
             std::string in_file = "data/leakyrelu_output_part" + std::to_string(i) + ".txt";
-            // std::string w_file  = "data/weights_dense2_part" + std::to_string(i) + ".txt";
+            std::string w_file  = "data/weights_dense2_part" + std::to_string(i) + ".txt";
 
-            // std::string in_name = "plio_input_dense2_" + std::to_string(i);
+            std::string in_name = "plio_input_dense2_" + std::to_string(i);
             std::string w_name  = "plio_weights_dense2_" + std::to_string(i);
 
-            // pl_in_dense2[i] = input_plio::create(in_name.c_str(), plio_32_bits, in_file.c_str());
+            pl_in_dense2[i] = input_plio::create(in_name.c_str(), plio_32_bits, in_file.c_str());
             pl_w_dense2[i]  = input_plio::create(w_name.c_str(), plio_32_bits, w_file.c_str());
 
-            // connect<>(pl_in_dense2[i].out[0], dense2.inB[i]);
             connect<>(pl_in_dense2[i].out[0], dense2.inB[i]);
             connect<>(pl_w_dense2[i].out[0], dense2.inA[i]);
         }
