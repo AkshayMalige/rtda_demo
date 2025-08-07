3e6cf6ebedc04617abf0eb92f276fffbcbd6fe90
amalige <amalige@bnl.gov>
2025-08-07 13:22:59 -0400

    added embed.ipynb

--- aieml/graph.h
+++ aieml/graph.h
@@ -80,7 +80,7 @@ public:
             connect<>(pl_w_dense2[i].out[0], dense2.inA[i]);
         }
 
-        pl_out_dense2 = output_plio::create("plio_output_dense2", plio_32_bits);
+        pl_out_dense2 = output_plio::create("plio_output_dense2", plio_32_bits,(base_path + "/dense2_output_aie.txt").c_str());
         connect<>(dense2.out[0], pl_out_dense2.in[0]);
     }
 };
