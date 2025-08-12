7a00cb81fea609e7ab2809a0c9e031b9b211a7b3
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-08-12 10:52:21 -0400

    Fix ONNX input padding in dump script

--- pl/src/leaky_relu_pl.cpp
+++ pl/src/leaky_relu_pl.cpp
@@ -9,17 +9,20 @@ typedef float data_t;
 typedef hls::axis<data_t, 0, 0, 0> axis_t;
 
 extern "C" {
-    // Apply leaky ReLU to SIZE elements from input to output stream
+    // Apply bias then leaky ReLU to SIZE elements
     void leaky_relu_pl(hls::stream<axis_t>& in_stream,
+                       hls::stream<axis_t>& bias_stream,
                        hls::stream<axis_t>& out_stream) {
         #pragma HLS interface axis port=in_stream bundle=gmem depth=128
+        #pragma HLS interface axis port=bias_stream bundle=gmem depth=128
         #pragma HLS interface axis port=out_stream bundle=gmem depth=128
         #pragma HLS INTERFACE s_axilite port=return bundle=control
 
         for (int i = 0; i < SIZE; i++) {
             #pragma HLS pipeline II=1
-            axis_t val = in_stream.read();
-            data_t input_val = val.data;
+            axis_t val       = in_stream.read();
+            axis_t bias_val  = bias_stream.read();
+            data_t input_val = val.data + bias_val.data;
             data_t output_val = (input_val >= 0) ? input_val : (input_val * LEAKY_SLOPE);
             val.data = output_val;
             out_stream.write(val);
