4508729d95201cf0a3c194d5013fea6fa070a0d6
amalige <amalige@bnl.gov>
2025-08-13 20:41:21 -0400

    some pl leaky_relu erre

--- common/data_paths.h
+++ common/data_paths.h
@@ -5,10 +5,12 @@
 
 #define INPUT_DATA_FILE "input.txt"
 #define WEIGHTS_DENSE1_FILE "dense_0_weights.txt"
+#define OUTPUT_DENSE1_FILE "dense_0_output_aie.txt"
+
 
 #define LEAKY_RELU_OUTPUT_PREFIX "leakyrelu_0_output_part"
 #define WEIGHTS_DENSE2_PREFIX "dense_1_weights_part"
 
-#define BIAS_DENSE1_FILE "bias_dense1.txt"
-#define BIAS_DENSE2_FILE "bias_dense2.txt"
+#define BIAS_DENSE1_FILE "dense_0_bias.txt"
+#define BIAS_DENSE2_FILE "dense_1_bias.txt"
 #define HOST_OUTPUT_FILE "host_output.txt"
