00dbc5417f0f96626d0072ac04847ed7c82fd2f6
amalige <amalige@bnl.gov>
2025-08-15 13:10:46 -0400

    embed block checked

--- aieml2/graph.h
+++ aieml2/graph.h
@@ -8,8 +8,8 @@ using namespace adf;
 using namespace xf::dsp::aie::blas::matrix_vector_mul;
 
 // switch graph to operate on float16 data
-using TT_DATA   = float16;
-using TT_WEIGHT = float16;
+using TT_DATA   = float;
+using TT_WEIGHT = float;
 
 // network dimensions
 static constexpr unsigned int INPUT_SIZE  = 768;
@@ -19,8 +19,8 @@ static constexpr unsigned int OUTPUT_SIZE = 128;
 static constexpr unsigned int TILE_BYTES          = 64 * 1024;
 static constexpr unsigned int BYTES_PER_WEIGHT   = sizeof(TT_WEIGHT);
 static constexpr unsigned int MAX_INPUTS_PER_TILE = TILE_BYTES / (BYTES_PER_WEIGHT * OUTPUT_SIZE);
-static constexpr unsigned int TP_CASC_LEN         = (INPUT_SIZE + MAX_INPUTS_PER_TILE - 1) / MAX_INPUTS_PER_TILE;
-static_assert(INPUT_SIZE % TP_CASC_LEN == 0, "Input size must divide cascade length");
+static constexpr unsigned int TP_CASC_LEN_768         = (INPUT_SIZE + MAX_INPUTS_PER_TILE - 1) / MAX_INPUTS_PER_TILE;
+static_assert(INPUT_SIZE % TP_CASC_LEN_768 == 0, "Input size must divide cascade length");
 
 static constexpr unsigned int TP_SHIFT          = 0;
 static constexpr unsigned int TP_RND            = rnd_floor;
@@ -36,21 +36,21 @@ using dense768x128 = matrix_vector_mul_graph<
     TP_SHIFT,
     TP_RND,
     TP_NUM_FRAMES,
-    TP_CASC_LEN,
+    TP_CASC_LEN_768,
     TP_SAT,
     TP_SSR,
     TP_DIM_A_LEADING>;
 
 class SingleDenseGraph : public graph {
 public:
-    input_plio  pl_in[TP_CASC_LEN];
-    input_plio  pl_w[TP_CASC_LEN];
+    input_plio  pl_in[TP_CASC_LEN_768];
+    input_plio  pl_w[TP_CASC_LEN_768];
     output_plio pl_out;
     dense768x128 dense;
 
     SingleDenseGraph() {
         std::string base_path = DATA_DIR;
-        for (int i = 0; i < TP_CASC_LEN; ++i) {
+        for (int i = 0; i < TP_CASC_LEN_768; ++i) {
             std::string in_file = base_path + "/input768_part" + std::to_string(i) + ".txt";
             std::string w_file  = base_path + "/weights768_part" + std::to_string(i) + ".txt";
             std::string in_name = "plio_input_" + std::to_string(i);
