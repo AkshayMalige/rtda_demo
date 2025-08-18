6c06a3592773c8648b86a9168ea7b5064a484328
amalige <amalige@bnl.gov>
2025-08-18 11:10:31 -0400

    working solver-0 graph block

---
+++ aieml2/include.h
@@ -0,0 +1,7 @@
+
+#pragma once
+
+#define HIDDEN_SIZE  128
+#define OUTPUT_SIZE  128
+#define LEAKY_SLOPE  0.1f
+
--- common/data_paths.h
+++ common/data_paths.h
@@ -3,16 +3,37 @@
 #define DATA_DIR "/home/synthara/VersalPrjs/LDRD/rtda_demo/data"
 #endif
 
-#define EMBED_INPUT_DATA "input.txt"
-#define EMBED_DENSE0_WEIGHTS "dense_0_weights.txt"
-#define EMBED_DENSE0_OUTPUT "dense_0_output_aie.txt"
+// #define EMBED_INPUT_DATA "input.txt"
+// #define EMBED_DENSE0_WEIGHTS "dense_0_weights.txt"
+// #define EMBED_DENSE0_OUTPUT "dense_0_output_aie.txt"
 
 
-#define EMBED_LEAKYRELU0_OUTPUT_PREFIX "leakyrelu_0_output_part"
-#define EMBED_DENSE1_WEIGHTS_PREFIX "dense_1_weights_part"
-#define EMBED_DENSE1_OUTPUT "dense_1_output_aie.txt"
+// #define EMBED_LEAKYRELU0_OUTPUT_PREFIX "leakyrelu_0_output_part"
+// #define EMBED_DENSE1_WEIGHTS_PREFIX "dense_1_weights_part"
+// #define EMBED_DENSE1_OUTPUT "dense_1_output_aie.txt"
 
 
-#define EMBED_DENSE0_BIAS "dense_0_bias.txt"
-#define EMBED_DENSE1_BIAS "dense_1_bias.txt"
-#define EMBED_HOST_OUTPUT "host_output.txt"
+// #define EMBED_DENSE0_BIAS "dense_0_bias.txt"
+// #define EMBED_DENSE1_BIAS "dense_1_bias.txt"
+// #define EMBED_HOST_OUTPUT "host_output.txt"
+
+
+#define SUBSOLVER0_INPUT_DATA_PREFIX "input_part"
+#define SUBSOLVER0_DENSE0_WEIGHTS_PREFIX "dense_0_weights_part"
+#define SUBSOLVER0_DENSE0_OUTPUT "dense_0_output_aie.txt"
+#define SUBSOLVER0_LEAKYRELU_0_PREFIX "leakyrelu_0_output_part"
+#define SUBSOLVER0_DENSE1_WEIGHTS_PREFIX "dense_1_weights_part"
+#define SUBSOLVER0_DENSE1_OUTPUT "dense_1_output_aie.txt"
+
+#define SUBSOLVER0_LEAKYRELU_1_PREFIX "leakyrelu_1_output_part"
+#define SUBSOLVER0_DENSE2_WEIGHTS_PREFIX "dense_2_weights_part"
+#define SUBSOLVER0_DENSE2_OUTPUT "dense_2_output_aie.txt"
+
+#define SUBSOLVER0_LEAKYRELU_2_PREFIX "leakyrelu_2_output_part"
+#define SUBSOLVER0_DENSE3_WEIGHTS_PREFIX "dense_3_weights_part"
+#define SUBSOLVER0_DENSE3_OUTPUT "dense_3_output_aie.txt"
+
+
+
+
+
