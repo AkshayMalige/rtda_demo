aa6e23078b336fcb1820b20fef4c07cad0f177b9
amalige <amalige@bnl.gov>
2025-08-19 16:04:54 -0400

    working, running and verified aieml, aieml2 and aieml3

--- common/data_paths.h
+++ common/data_paths.h
@@ -6,16 +6,11 @@
 #define EMBED_INPUT_DATA "embed_input.txt"
 #define EMBED_DENSE0_WEIGHTS "embed_dense_0_weights.txt"
 #define EMBED_DENSE0_OUTPUT "embed_dense_0_output_aie.txt"
-
 #define EMBED_LEAKYRELU0_OUTPUT_PREFIX "embed_leakyrelu_0_output_part"
 #define EMBED_DENSE1_WEIGHTS_PREFIX "embed_dense_1_weights_part"
 #define EMBED_DENSE1_OUTPUT "embed_dense_1_output_aie.txt"
-
 #define EMBED_DENSE0_BIAS "embed_dense_0_bias.txt"
 #define EMBED_DENSE1_BIAS "embed_dense_1_bias.txt"
-
-
-
 
 
 #define SUBSOLVER0_INPUT_DATA_PREFIX "solver_0_input_part"
@@ -24,22 +19,17 @@
 #define SUBSOLVER0_LEAKYRELU_0_PREFIX "solver_0_leakyrelu_0_output_part"
 #define SUBSOLVER0_DENSE1_WEIGHTS_PREFIX "solver_0_dense_1_weights_part"
 #define SUBSOLVER0_DENSE1_OUTPUT "solver_0_dense_1_output_aie.txt"
-
 #define SUBSOLVER0_LEAKYRELU_1_PREFIX "solver_0_leakyrelu_1_output_part"
 #define SUBSOLVER0_DENSE2_WEIGHTS_PREFIX "solver_0_dense_2_weights_part"
 #define SUBSOLVER0_DENSE2_OUTPUT "solver_0_dense_2_output_aie.txt"
-
 #define SUBSOLVER0_LEAKYRELU_2_PREFIX "solver_0_leakyrelu_2_output_part"
 #define SUBSOLVER0_DENSE3_WEIGHTS_PREFIX "solver_0_dense_3_weights_part"
 #define SUBSOLVER0_DENSE3_OUTPUT "solver_0_dense_3_output_aie.txt"
 
 
-
-
-#define OUTPUT_INPUT_DATA "input.txt"
-#define OUTPUT_DENSE0_WEIGHTS "dense_0_weights.txt"
-#define OUTPUT_DENSE0_OUTPUT "dense_0_output_aie.txt"
-
+#define OUTPUT_INPUT_DATA "output_input.txt"
+#define OUTPUT_DENSE0_WEIGHTS "output_dense_0_weights.txt"
+#define OUTPUT_DENSE0_OUTPUT "output_dense_0_output_aie.txt"
 
 
 #define HOST_OUTPUT "host_output.txt"
