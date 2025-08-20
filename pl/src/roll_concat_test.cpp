72448f1f720a6848807c2b52e0c39d4b0ec9721d
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-08-20 18:22:16 -0400

    Rename roll_concat kernel top function

--- pl/src/roll_concat_test.cpp
+++ pl/src/roll_concat_test.cpp
@@ -1,12 +1,13 @@
 #include <hls_stream.h>
 #include <iostream>
 #include <cmath>
+#include <fstream>
 #include "../../common/nn_defs.h"
 
 #define OUTPUT_SIZE (HIDDEN_SIZE * ROLL_CONC_SUBSET_SIZE)
 
 typedef float data_t;
-void roll_concat(hls::stream<data_t> &in, hls::stream<data_t> &out);
+void roll_concat_pl(hls::stream<data_t> &in, hls::stream<data_t> &out);
 
 // Golden model
 void reference_model(data_t in[HIDDEN_SIZE], data_t out[OUTPUT_SIZE]) {
@@ -26,14 +27,20 @@ int main() {
     data_t ref_output[OUTPUT_SIZE];
     data_t test_output[OUTPUT_SIZE];
 
-    // Initialize input with known pattern
+    // Read input vector from file
+    std::ifstream infile("../data/embed_model_output.txt");
+    if (!infile) {
+        std::cerr << "Failed to open input file" << std::endl;
+        return 1;
+    }
     for (int i = 0; i < HIDDEN_SIZE; i++) {
-        input[i] = (data_t)i;
+        infile >> input[i];
         in_stream.write(input[i]);
     }
+    infile.close();
 
     // Run kernel
-    roll_concat(in_stream, out_stream);
+    roll_concat_pl(in_stream, out_stream);
 
     // Capture output
     for (int i = 0; i < OUTPUT_SIZE; i++) {
