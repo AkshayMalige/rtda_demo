5b9fa4e25a3ce8e12d6441264e1cfc7427558008
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-08-22 12:44:03 -0400

    Replace array-indexed output with switch

--- pl/src/axis_switch_pl.cpp
+++ pl/src/axis_switch_pl.cpp
@@ -20,7 +20,61 @@ void axis_switch_pl(hls::stream<axis_t> &in, hls::stream<axis_t> out[NUM_OUTPUTS
         axis_t val = in.read();
         ap_uint<5> dest = val.dest;
         if (dest < NUM_OUTPUTS) {
-            out[dest].write(val);
+            switch (dest) {
+            case 0:
+                out[0].write(val);
+                break;
+            case 1:
+                out[1].write(val);
+                break;
+            case 2:
+                out[2].write(val);
+                break;
+            case 3:
+                out[3].write(val);
+                break;
+            case 4:
+                out[4].write(val);
+                break;
+            case 5:
+                out[5].write(val);
+                break;
+            case 6:
+                out[6].write(val);
+                break;
+            case 7:
+                out[7].write(val);
+                break;
+            case 8:
+                out[8].write(val);
+                break;
+            case 9:
+                out[9].write(val);
+                break;
+            case 10:
+                out[10].write(val);
+                break;
+            case 11:
+                out[11].write(val);
+                break;
+            case 12:
+                out[12].write(val);
+                break;
+            case 13:
+                out[13].write(val);
+                break;
+            case 14:
+                out[14].write(val);
+                break;
+            case 15:
+                out[15].write(val);
+                break;
+            case 16:
+                out[16].write(val);
+                break;
+            default:
+                break;
+            }
         }
         last = val.last;
     }
