07370e44896c043268db7a6729b0244a2d18a378
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-08-24 16:39:03 -0400

    Preload bias data into BRAM buffer

--- pl/src/leaky_relu_pl.cpp
+++ pl/src/leaky_relu_pl.cpp
@@ -18,11 +18,19 @@ extern "C" {
         #pragma HLS INTERFACE s_axilite port=size bundle=control
         #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-        for (int i = 0; i < size; i++) {
+        data_t bias_buf[HIDDEN_SIZE];
+        #pragma HLS BIND_STORAGE variable=bias_buf type=ram_1p impl=bram
+
+        preload_loop: for (int i = 0; i < size; i++) {
+            #pragma HLS PIPELINE II=1
+            axis_t bias_val = bias_stream.read();
+            bias_buf[i] = bias_val.data;
+        }
+
+        main_loop: for (int i = 0; i < size; i++) {
             #pragma HLS pipeline II=1
-            axis_t val       = in_stream.read();
-            axis_t bias_val  = bias_stream.read();
-            data_t input_val = val.data + bias_val.data;
+            axis_t val = in_stream.read();
+            data_t input_val = val.data + bias_buf[i];
             data_t output_val = (input_val >= 0) ? input_val : (input_val * LEAKY_SLOPE);
             val.data = output_val;
             out_stream.write(val);
