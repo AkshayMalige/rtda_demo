0a876d39f13ae1ab8345e1b7c774d0cfdb1fb1cb
amalige <amalige@bnl.gov>
2025-08-25 13:05:56 -0400

    simpler demux

--- pl/src/demux_8_pl.cpp
+++ pl/src/demux_8_pl.cpp
@@ -2,59 +2,22 @@
 #include <hls_stream.h>
 #include <ap_axi_sdata.h>
 
-template<int M>
-struct Demux {
-  typedef ap_axiu<32,1,1,8> axis_t; // data,user,id,dest
-
-  static void run(hls::stream<axis_t>& in,
-                  hls::stream<axis_t> out[M]) {
-#pragma HLS INLINE off
-#pragma HLS ARRAY_PARTITION variable=out complete dim=1
-
-    bool have_route = false;
-    ap_uint<8> route = 0;
-
-    while (1) {
-#pragma HLS PIPELINE II=1
-      if (!in.empty()) {
-        axis_t t = in.read();
-
-        // Latch route at packet start (first beat after TLAST or reset)
-        if (!have_route) {
-          route = t.dest;
-          have_route = true;
-        }
-
-        // Route selection (clamp to range 0..M-1)
-        ap_uint<8> r = (route < (ap_uint<8>)M) ? route : (ap_uint<8>)0;
-
-        // Forward beat to selected output
-        axis_t o = t;
-        // Clear meta fields we don't use downstream
-        o.id = 0; o.user = 0; o.dest = 0;
-        out[r].write(o);
-
-        // End-of-packet: reset routing latch
-        if (t.last) {
-          have_route = false;
-        }
-      }
-    }
-  }
-};
+// 32-bit data, 1-bit user, 1-bit id, 8-bit dest
+typedef ap_axiu<32,1,1,8> axis_t;
 
 extern "C" {
-void weights_demux_8( // example with 8 outputs; make variants if you need more
-  hls::stream<ap_axiu<32,1,1,8>>& in,
-  hls::stream<ap_axiu<32,1,1,8>>& out0,
-  hls::stream<ap_axiu<32,1,1,8>>& out1,
-  hls::stream<ap_axiu<32,1,1,8>>& out2,
-  hls::stream<ap_axiu<32,1,1,8>>& out3,
-  hls::stream<ap_axiu<32,1,1,8>>& out4,
-  hls::stream<ap_axiu<32,1,1,8>>& out5,
-  hls::stream<ap_axiu<32,1,1,8>>& out6,
-  hls::stream<ap_axiu<32,1,1,8>>& out7
+void demux_8(
+  hls::stream<axis_t>& in,
+  hls::stream<axis_t>& out0,
+  hls::stream<axis_t>& out1,
+  hls::stream<axis_t>& out2,
+  hls::stream<axis_t>& out3,
+  hls::stream<axis_t>& out4,
+  hls::stream<axis_t>& out5,
+  hls::stream<axis_t>& out6,
+  hls::stream<axis_t>& out7
 ) {
+  // AXI-Stream ports
 #pragma HLS INTERFACE axis port=in
 #pragma HLS INTERFACE axis port=out0
 #pragma HLS INTERFACE axis port=out1
@@ -64,22 +27,57 @@ void weights_demux_8( // example with 8 outputs; make variants if you need more
 #pragma HLS INTERFACE axis port=out5
 #pragma HLS INTERFACE axis port=out6
 #pragma HLS INTERFACE axis port=out7
-#pragma HLS INTERFACE s_axilite port=return bundle=control
+  // Free-running kernel (no AXI-Lite control FSM)
+#pragma HLS INTERFACE ap_ctrl_none port=return
 
-  hls::stream<ap_axiu<32,1,1,8>> outs[8];
-#pragma HLS STREAM variable=outs depth=32
-#pragma HLS ARRAY_PARTITION variable=outs complete dim=1
+  // Optional elasticity on ports (tune if you see back-pressure)
+#pragma HLS STREAM variable=out0 depth=64
+#pragma HLS STREAM variable=out1 depth=64
+#pragma HLS STREAM variable=out2 depth=64
+#pragma HLS STREAM variable=out3 depth=64
+#pragma HLS STREAM variable=out4 depth=64
+#pragma HLS STREAM variable=out5 depth=64
+#pragma HLS STREAM variable=out6 depth=64
+#pragma HLS STREAM variable=out7 depth=64
 
-  Demux<8>::run(in, outs);
+  bool       have_route = false;
+  ap_uint<8> route      = 0;
 
-  // Bridge array to individual AXIS ports
-  if (!outs[0].empty()) out0.write(outs[0].read());
-  if (!outs[1].empty()) out1.write(outs[1].read());
-  if (!outs[2].empty()) out2.write(outs[2].read());
-  if (!outs[3].empty()) out3.write(outs[3].read());
-  if (!outs[4].empty()) out4.write(outs[4].read());
-  if (!outs[5].empty()) out5.write(outs[5].read());
-  if (!outs[6].empty()) out6.write(outs[6].read());
-  if (!outs[7].empty()) out7.write(outs[7].read());
+  while (1) {
+#pragma HLS PIPELINE II=1
+    // Blocking read simplifies logic and helps the scheduler
+    axis_t t = in.read();
+
+    // On the first beat of a packet, latch TDEST as the route
+    if (!have_route) {
+      route = t.dest;
+      have_route = true;
+    }
+
+    // Prepare output beat: preserve data/keep/strb/last; clear meta we don't use
+    axis_t o = t;
+    o.id   = 0;
+    o.user = 0;
+    o.dest = 0; // we've already consumed TDEST into 'route'
+
+    // Clamp to 0..7 (unknown TDESTs go to out7 by default)
+    ap_uint<8> r = (route < 8) ? route : (ap_uint<8>)7;
+
+    switch (r) {
+      case 0: out0.write(o); break;
+      case 1: out1.write(o); break;
+      case 2: out2.write(o); break;
+      case 3: out3.write(o); break;
+      case 4: out4.write(o); break;
+      case 5: out5.write(o); break;
+      case 6: out6.write(o); break;
+      default: out7.write(o); break;
+    }
+
+    // End of packet → unlock route for next packet
+    if (t.last) {
+      have_route = false;
+    }
+  }
 }
 }
