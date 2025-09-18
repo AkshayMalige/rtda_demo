279de5acd08f8446f0c94f41369dd919bbe4ddb6
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-09-18 19:26:02 -0400

    Fix HLS stream selection in packet sender

--- pl/hls_packet_receiver.cpp
+++ pl/hls_packet_receiver.cpp
@@ -11,27 +11,27 @@ static const int PACKET_NUM=4;
 
 static const unsigned int packet_ids[PACKET_NUM]={Dataout0_0, Dataout0_1, Dataout0_2, Dataout0_3};
 
+typedef ap_axiu<32,0,0,0> axis32_t;
+
 unsigned int getPacketId(ap_uint<32> header){
 #pragma HLS inline
-	ap_uint<32> ID=0;
-	ID(7,0)=header(7,0);
-	return ID;
+        ap_uint<32> ID=0;
+        ID(7,0)=header(7,0);
+        return ID;
 }
 
-void hls_packet_receiver(hls::stream<ap_axiu<32,0,0,0>> &in, hls::stream<ap_axiu<32,0,0,0>> &out0,hls::stream<ap_axiu<32,0,0,0>> &out1,hls::stream<ap_axiu<32,0,0,0>> &out2,hls::stream<ap_axiu<32,0,0,0>> &out3,
-	const unsigned int total_num_packet){
-        for(unsigned int iter=0;iter<total_num_packet;iter++){
-                ap_axiu<32,0,0,0> header=in.read();//first word is packet header
+void hls_packet_receiver(hls::stream<axis32_t> &in, hls::stream<axis32_t> &out0,hls::stream<axis32_t> &out1,hls::stream<axis32_t> &out2,hls::stream<axis32_t> &out3,
+        const unsigned int total_num_packet){
+        const int packet_limit = static_cast<int>(total_num_packet);
+        for(int pkt=0; pkt<packet_limit; ++pkt){
+                axis32_t header=in.read();//first word is packet header
                 unsigned int ID=getPacketId(header.data);
-                unsigned int channel=0;
                 bool valid_channel = (ID < PACKET_NUM);
-                if(valid_channel){
-                        channel=packet_ids[ID];
-                }
+                unsigned int channel = valid_channel ? packet_ids[ID] : 0;
 
                 bool last_word=false;
                 do{
-                        ap_axiu<32,0,0,0> tmp=in.read();
+                        axis32_t tmp=in.read();
                         last_word = tmp.last;
                         if(valid_channel){
                                 switch(channel){
@@ -39,6 +39,7 @@ void hls_packet_receiver(hls::stream<ap_axiu<32,0,0,0>> &in, hls::stream<ap_axiu
                                 case 1:out1.write(tmp);break;
                                 case 2:out2.write(tmp);break;
                                 case 3:out3.write(tmp);break;
+                                default:break;
                                 }
                         }
                 }while(!last_word);
