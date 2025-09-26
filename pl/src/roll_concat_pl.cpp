8a87827d823082bc7e0cd69afa5d47543c73139c
amalige <amalige@bnl.gov>
2025-09-26 14:37:17 +0000

    tmp

--- pl/src/roll_concat_pl.cpp
+++ pl/src/roll_concat_pl.cpp
@@ -20,7 +20,7 @@ void roll_concat_pl(hls::stream<data_t> &in, hls::stream<data_t> &out) {
     }
 
     // Emit ROLL_CONC_SUBSET_SIZE shifted versions
-    for (int shift = 0; shift < ROLL_CONC_SUBSET_SIZE; shift++) {
+    for (int shift = 0; shift < ROLL_CONC_SUBSET_SIZE; shift++) { //constexpr int ROLL_CONC_SUBSET_SIZE = 6;
         for (int i = 0; i < HIDDEN_SIZE; i++) {
 #pragma HLS PIPELINE II=1
             int idx = (i + shift) % HIDDEN_SIZE;
