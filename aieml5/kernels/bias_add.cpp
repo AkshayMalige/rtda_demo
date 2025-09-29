1c5eef16d3b7b0c57f70a3982c77e61eef903f13
akshay malige <38230698+AkshayMalige@users.noreply.github.com>
2025-09-29 12:13:56 -0400

    Use __restrict qualifiers for bias kernel

--- aieml5/kernels/bias_add.cpp
+++ aieml5/kernels/bias_add.cpp
@@ -1,9 +1,9 @@
 #include "bias_add.h"
 #include "nn_defs.h"
 
-void bias_add_kernel(input_stream<float>* restrict dense_output,
-                     input_stream<float>* restrict bias_stream,
-                     output_stream<float>* restrict biased_output)
+void bias_add_kernel(input_stream<float>* __restrict dense_output,
+                     const float* __restrict bias,
+                     output_stream<float>* __restrict biased_output)
 {
     v16float bias_vec = aie::zeros<float, 16>();
     v16float dense_vec = aie::zeros<float, 16>();
@@ -12,7 +12,7 @@ void bias_add_kernel(input_stream<float>* restrict dense_output,
     // Process HIDDEN_SIZE (128) elements in chunks of 16
     for (int i = 0; i < HIDDEN_SIZE; i += 16) {
         dense_vec = readincr_v16(dense_output);
-        bias_vec = readincr_v16(bias_stream);
+        bias_vec = aie::load_v<16>(&bias[i]);
 
         result_vec = aie::add(dense_vec, bias_vec);
 
--- aieml5/kernels/bias_add.h
+++ aieml5/kernels/bias_add.h
@@ -1,10 +1,8 @@
 #pragma once
-
 #include <aie_api/aie.hpp>
 #include <aie_api/aie_adf.hpp>
-
 using namespace aie;
+void bias_add_kernel(input_stream<float>* __restrict dense_output,
+                     const float* __restrict bias,
+                     output_stream<float>* __restrict biased_output);
 
-void bias_add_kernel(input_stream<float>* restrict dense_output,
-                     input_stream<float>* restrict bias_stream,
-                     output_stream<float>* restrict biased_output);
