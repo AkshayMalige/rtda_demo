74ce2896d67342ef1c9cd177c8d037ba894fd3b7
amalige <amalige@bnl.gov>
2025-10-06 19:57:29 +0000

    removed aieml9

--- aieml9/window_split_128_to_64x2.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <adf.h>
-using namespace adf;
-
-void window_split_128_to_64x2(input_window<float>* __restrict in,
-                              output_window<float>* __restrict out0,
-                              output_window<float>* __restrict out1);
