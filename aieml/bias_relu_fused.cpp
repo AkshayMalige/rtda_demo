9ae5759d426235279377a82b9d98903403297326
amalige <amalige@bnl.gov>
2025-10-29 09:00:29 -0400

    fixed bias add window size

--- aieml/bias_relu_fused.cpp
+++ aieml/bias_relu_fused.cpp
@@ -4,7 +4,7 @@
 void bias_add_leaky_relu_kernel(input_window<float>* __restrict in,
                                 output_window<float>* __restrict out,
                                 const float (&bias)[HIDDEN_SIZE]) {
-    const int N = window_size(in) / sizeof(float);   // exact elements in this window
+    const int N = window_size(in);   // exact elements in this window
     // If bias is only HIDDEN_SIZE long but N may be padded, clamp N to HIDDEN_SIZE
     const int M = (N < HIDDEN_SIZE) ? N : HIDDEN_SIZE;
 
