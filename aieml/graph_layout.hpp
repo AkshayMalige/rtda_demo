fc0e80353eaf412e58089ed1a833542c5ceda65e
amalige <amalige@bnl.gov>
2026-02-11 17:53:18 +0000

    added graph layout

---
+++ aieml/graph_layout.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+inline void NeuralNetworkGraph::apply_layout() {
+
+    // Embed stage
+    runtime<ratio>(embed_bias_relu0) = 0.95;
+    runtime<ratio>(embed_split0)     = 0.95;
+    runtime<ratio>(embed_bias_relu1) = 0.95;
+
+    // Solver0 stage
+    runtime<ratio>(solver0_rollconcat) = 1.0;
+
+    runtime<ratio>(solver0_bias_relu0) = 0.95;
+    runtime<ratio>(solver0_split0)     = 0.65;
+    runtime<ratio>(solver0_bias_relu1) = 0.95;
+    runtime<ratio>(solver0_split1)     = 0.65;
+    runtime<ratio>(solver0_bias_relu2) = 0.95;
+    runtime<ratio>(solver0_split2)     = 0.65;
+    runtime<ratio>(solver0_bias_relu3) = 0.95;
+}
